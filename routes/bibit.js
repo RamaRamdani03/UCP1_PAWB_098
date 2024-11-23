246ab236d5ad06315daf9af803de9b8258e93fe0
RamaRamdani03 <ramaramdani449@gamil.com>
2024-11-23 20:18:27 +0700

    inisialisasi router Express untuk rute baru

--- routes/bibit.js
+++ routes/bibit.js
@@ -1,20 +1,2 @@
-import express, {Router} from "express";
+import express, { Router } from "express";
 const router = express.Router();
-const bibit = [
-{
-    namaBibit: "Padi Unggul",
-    Jenis_Tanaman: "Padi",
-    Harga: "5000",
-    Stok: "100",
-},
-{
-    namaBibit: "Jagung manis",
-    Jenis_Tanaman: "Jagung",
-    Harga: "10000",
-    Stok: "200",   
-},
-];
-router.get("/", (req, res) => {
-    res.send(bibit);
-})
-export default router;
