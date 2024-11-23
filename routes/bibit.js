5e0474878628f0f1ed1df4ee212230d05f8534b2
RamaRamdani03 <ramaramdani449@gamil.com>
2024-11-23 20:19:48 +0700

    tambahkan data bibit awal dengan informasi padi dan jagung

--- routes/bibit.js
+++ routes/bibit.js
@@ -1,2 +1,19 @@
 import express, { Router } from "express";
 const router = express.Router();
+
+const bibit = [
+    {
+        id: 1,
+        namaBibit: "Padi Unggul",
+        Jenis_Tanaman: "Padi",
+        Harga: "5000",
+        Stok: "100",
+    },
+    {
+        id: 2,
+        namaBibit: "Jagung manis",
+        Jenis_Tanaman: "Jagung",
+        Harga: "10000",
+        Stok: "200",
+    },
+];
