105bf960b8e8f958e672d4cd53a149f3e105f2be
RamaRamdani03 <ramaramdani449@gamil.com>
2024-11-23 20:26:46 +0700

    Menambahkan data awal untuk pupuk dengan informasi Rama Urea dan Kandang

--- routes/pupuk.js
+++ routes/pupuk.js
@@ -1,2 +1,18 @@
 import express, { Router } from "express";
 const router = express.Router();
+const pupuk = [
+    {
+        id: 1,
+        NamaPupuk: "Rama Urea",
+        JenisPupuk: "Kimia",
+        Harga: "15000",
+        Stok: "250",
+    },
+    {
+        id: 2,
+        NamaPupuk: "Kandang",
+        JenisPupuk: "Organik",
+        Harga: "13000",
+        Stok: "25000",
+    }
+];
