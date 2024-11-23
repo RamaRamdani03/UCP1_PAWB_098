9f3a4c98ca335282200c76d4e2e89d755799c166
RamaRamdani03 <ramaramdani449@gamil.com>
2024-11-23 20:06:54 +0700

    server Express dengan rute untuk bibit dan pupuk

--- index.js
+++ index.js
@@ -1,21 +1,2 @@
 import express from "express";
-import bodyParser from 'body-parser';
-import bibitRoute from "./routes/bibit.js";
-import pupukRoute from "./routes/pupuk.js";
 
-const app = express();
-const port = 8000;
-
-app.use(bodyParser.json());
-
-app.use("/bibit", bibitRoute);
-app.use("/pupuk", pupukRoute);
-app.get("/", (req, res) => {
-    console.log(["Get ROUTE"]);
-    res.send("Selamat Pagiiiii");
-});
-
-app.listen(port, () =>
-    console.log(
-        `Server berjalan di port: http://localhost:${port}`)
-);
